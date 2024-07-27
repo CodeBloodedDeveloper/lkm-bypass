54ff18e9e3b1433eea28f1f91dc5f831699d06ea
Harshil Awasthi <96411822+CodeBloodedDeveloper@users.noreply.github.com>
2024-07-27 22:06:52 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,9 @@
 import pyautogui
 import time
 
-time.sleep(3)
-with open('code.txt','r')as f:
-    lines=f.readlines()
+time.sleep(7)
+with open('code.txt', 'r') as f:
+    lines = f.readlines()
     for line in lines:
         time.sleep(0.1)
-        pyautogui.write(line.lstrip())
-       
-       
+        pyautogui.write(line.lstrip().rstrip() + '\n')  
